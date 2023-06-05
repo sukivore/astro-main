4a5cb53214333453aadf7d92e939f3a0523640a7
sukivore <93158282+sukivore@users.noreply.github.com>
2023-06-05 22:59:01 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	  output: 'server',
+  adapter: netlify(),
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
